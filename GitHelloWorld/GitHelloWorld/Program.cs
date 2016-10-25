cc509d87e40242c052e9b131f7ba1fe2b29b35d5
Jimizzle <alfarshear981@gmail.com>
2016-10-25 19:12:56 -0400

    Adding a small C# console app, updated README

---
+++ GitHelloWorld/GitHelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is just to test and show that there's a program in the repository.");
+            Console.WriteLine("Now close this and gtfo already.");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Testing because I'm an idiot nub at Git
 
 This is an edit to the original post, practicing commits on a text file of all things.
+
+----------
+10/25/2016
+----------
+- Added a small console app to test the repo shit with GitHub. I made the console app, copied the contents from the original project, then pasted it into the git area.
