a3605cbcb8b57980f1478caf4d0086ab5af41fb9
Jimizzle <alfarshear981@gmail.com>
2016-10-25 19:32:30 -0400

    Updating the console app so it asks your name

--- GitHelloWorld/GitHelloWorld/Program.cs
+++ GitHelloWorld/GitHelloWorld/Program.cs
@@ -12,6 +12,10 @@ namespace GitHelloWorld
         {
             Console.WriteLine("This is just to test and show that there's a program in the repository.");
             Console.WriteLine("Now close this and gtfo already.");
+            Console.WriteLine("This is a new line in here just added in this particular new branch!");
+            Console.WriteLine("Now tell us your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ok, so your name is " + name + "?");
             Console.ReadKey();
         }
     }
